99db35e0abdbb8f6e38283085bc3cf9be6054c09
h4x <h4x3rotab@gmail.com>
2017-09-12 01:54:25 +0800

    PoW: Add dependency libsdoium for equihash.
    Tested:
    make (macOS 10.12.5, Xcode 8.3.3)

---
+++ depends/packages/libsodium.mk
@@ -0,0 +1,28 @@
+# Copyright (c) 2016 The Zcash developers
+# Copyright (c) 2017 The BTCGPU developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package=libsodium
+$(package)_version=1.0.13
+$(package)_download_path=https://download.libsodium.org/libsodium/releases
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=9c13accb1a9e59ab3affde0e60ef9a2149ed4d6e8f99c93c7a5b97499ee323fd
+$(package)_dependencies=
+$(package)_config_opts=
+
+define $(package)_preprocess_cmds
+  cd $($(package)_build_subdir); ./autogen.sh
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf) --enable-static --disable-shared
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
