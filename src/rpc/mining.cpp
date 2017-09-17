5c31b21bd2b88159f78cc76d7c1172624edeaadd
TheSorrow1 <31935646+TheSorrow1@users.noreply.github.com>
2017-09-17 16:12:17 +0200

    Update mining.cpp

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -226,7 +226,7 @@ UniValue getmininginfo(const JSONRPCRequest& request)
 }
 
 
-// NOTE: Unlike wallet RPC (which use BTC values), mining RPCs follow GBT (BIP 22) in using satoshi amounts
+// NOTE: Unlike wallet RPC (which use BTG values), mining RPCs follow GBT (BIP 22) in using satoshi amounts
 UniValue prioritisetransaction(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 3)
