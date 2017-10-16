7963a6e86c300ebdfc55787469442df573128ce0
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-10-17 02:59:09 +0300

    Removed unnecessary assertion line in the regtest config

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -294,7 +294,6 @@ public:
         consensus.BTGPremineWindow = 16000;
         consensus.powLimit = uint256S("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowAveragingWindow = 17;
-        assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
         consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
         consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
         consensus.nPowTargetTimespanLegacy = 14 * 24 * 60 * 60; // two weeks
