6aceb68e698c13abe8bae78149291f97eea27fff
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-10-20 18:10:54 +0300

    Removed main seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -159,7 +159,10 @@ public:
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         // TODO: Shall change this to our
+<<<<<<< HEAD
 
+=======
+>>>>>>> Removed main seeds
 //        vSeeds.emplace_back("seed.bitcoin.sipa.be", true); // Pieter Wuille, only supports x1, x5, x9, and xd
 //        vSeeds.emplace_back("dnsseed.bluematt.me", true); // Matt Corallo, only supports x9
 //        vSeeds.emplace_back("dnsseed.bitcoin.dashjr.org", false); // Luke Dashjr
@@ -223,10 +226,12 @@ public:
         consensus.BIP34Hash = uint256S("0x0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8");
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
+        
         consensus.BTGHeight = 1210320;
         consensus.BTGPremineWindow = 200;
         consensus.powLimit = uint256S("07ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimitLegacy = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
         consensus.nPowAveragingWindow = 17;
         assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
         consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
@@ -266,10 +271,7 @@ public:
         pchMessageStart[1] = 0x47;
         pchMessageStart[2] = 0x6d;
         pchMessageStart[3] = 0x44;
-<<<<<<< HEAD
         
-=======
->>>>>>> Changed minimum difficulty.
         nDefaultPort = 18338;
         nPruneAfterHeight = 1000;
         const size_t N = 200, K = 9;  // Same as mainchain.
