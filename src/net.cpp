0fcc5df9a3ef5a9044a71cee895390a7fe578031
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2017-10-29 23:49:27 +0200

    Fixes not working magic

--- src/net.cpp
+++ src/net.cpp
@@ -2731,7 +2731,7 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     nPingUsecStart = 0;
     nPingUsecTime = 0;
     fPingQueued = false;
-    fUsesGoldMagic = false;
+    fUsesGoldMagic = true;
     nMinPingUsecTime = std::numeric_limits<int64_t>::max();
     minFeeFilter = 0;
     lastSentFeeFilter = 0;
