cb74c1026b83ab6bc60f55bcbd1fb11bcd93d2c3
h4x <h4x3rotab@gmail.com>
2017-10-31 03:04:56 +0800

    Misc fixes.
    - Change the premine whitelist placeholder to BTG address format.
    - Reduce testnet premine window to 200 blocks.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -199,7 +199,7 @@ public:
         };
 
         vPremineAddressWhitelist = {
-            "3CjLoKt9KYcfjf4MWAbGz8xnpfJvzvMxMi",
+            "ASpCXHFL6nxSTTZuwib1iPrx9jwuia5HLQ",
         };
     }
 };
@@ -217,7 +217,7 @@ public:
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
         consensus.BTGHeight = 1210320;
-        consensus.BTGPremineWindow = 16000;
+        consensus.BTGPremineWindow = 200;
         consensus.powLimit = uint256S("07ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimitLegacy = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowAveragingWindow = 17;
