5a5e20b17096fe509431a8b6a40dced225c3f17c
h4x <h4x3rotab@gmail.com>
2017-10-30 22:30:08 +0800

    Change testnet fork height to 1210320.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -216,7 +216,7 @@ public:
         consensus.BIP34Hash = uint256S("0x0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8");
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
-        consensus.BTGHeight = 1210647; // 10/20/2017 12:00 UTC.
+        consensus.BTGHeight = 1210320;
         consensus.BTGPremineWindow = 16000;
         consensus.powLimit = uint256S("07ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimitLegacy = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
