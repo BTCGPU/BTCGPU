5a726f7c043b11460a8ffac296f81595e2df9b6d
h4x3rotab <h4x3rotab@gmail.com>
2019-08-28 12:59:09 +0800

    Fix NetworkHashPS defualt lookup window

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -59,7 +59,7 @@ static UniValue GetNetworkHashPS(int lookup, int height) {
 
     // If lookup is -1, then use blocks since last difficulty change.
     if (lookup <= 0)
-        lookup = pb->nHeight % Params().GetConsensus().DifficultyAdjustmentInterval() + 1;
+        lookup = Params().GetConsensus().nZawyLwmaAveragingWindow;
 
     // If lookup is larger than chain, then set it to chain length.
     if (lookup > pb->nHeight)
